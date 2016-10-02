d914b0ddee09b099457306ee4a4747887ca537fa
sratnakar <karthik@scalabledatatech.com>
2016-10-03 01:33:18 +0530

    Update TestApplication.java

--- sdt-toy-simulator/src/test/java/sdt/toy/weather/app/TestApplication.java
+++ sdt-toy-simulator/src/test/java/sdt/toy/weather/app/TestApplication.java
@@ -13,12 +13,12 @@ public class TestApplication {
 	@Test
 	public void testWeatherDetails() throws Exception {
 		
-		String expected = "Kolkata|22.54399,88.37908,3.023699283599854|2016-09-17T12:30Z|Rain|30|493.11901680266385|76.0";
+		String expected = "Kolkata|22.54994,88.37158,9.730396270751953";
 		YahooWeatherService service = new YahooWeatherService();
 		
 		String location = service.getWoeid("kolkata");
 		Channel channel = service.getForecast(location, DegreeUnit.CELSIUS);
-		String actual = Application.getWeatherDetails(service, channel);
+		String actual = Application.getlocLatLong(service, channel);
 		assertEquals(actual, expected);
 	}
 
